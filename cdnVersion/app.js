68e119a99b5622aa713cbe440a4e974971832f67
waleedyousaf07 <waleedyousaf07@gmail.com>
2022-02-10 14:37:59 +0500

    moves components and refs to cdn version files and updates css to style first level divs

--- cdnVersion/app.js
+++ cdnVersion/app.js
@@ -150,3 +150,50 @@ var two = new Vue({
 });
 
 two.title = 'Title changed from outside';
+
+Vue.component('greeting', { // Name of the component
+  template: '<p>Heyy there, Im {{ name }}. <button @click="changeName">Change Name</button></p>',
+  data: function () { // data property is not directly an object unlike Vue instance. Its a function because multiple Vue instances are gonna use this component so they would point/reference to same object, by using function, a fresh copy is made for the data
+    return {
+      name: 'Batman',
+    }
+  },
+  methods: {
+    changeName: function () { // will call this in the above template's button
+      this.name = 'Bruce';
+    },
+  }
+});
+
+new Vue({
+  el: '#vue-app-one',
+  data: {
+    title: 'Vue App One',
+  },
+  methods: {},
+  computed: {},
+});
+
+new Vue({
+  el: '#vue-app-two',
+  data: {
+    title: 'Vue App Two',
+  },
+  methods: {},
+  computed: {},
+});
+
+new Vue({
+  el: '#refs',
+  data: {
+    output: 'Your fav food',
+  },
+  methods: {
+    readRefs: function () {
+      console.log(this.$refs); // this.$refs will give us an object with all the refs on the page, the key will be the ref's name, and value would be the element and alot of properties on that
+      console.log(this.$refs.test.innerText);
+      this.output = this.$refs.inputRef.value;
+    },
+  },
+  computed: {},
+});
