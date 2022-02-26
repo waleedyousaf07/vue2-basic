015683d1e803684987c9c81fd37f01ea0fa1ebb2
waleedyousaf07 <waleedyousaf07@gmail.com>
2022-02-26 23:00:58 +0500

    adds single blog page

---
+++ cliVersion/src/components/blog/SingleBlog.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="single-blog">
+    <h1>{{ blog.title }}</h1>
+    <article>{{ blog.body }}</article>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  data () {
+    return {
+      blog: {},
+    }
+  },
+  created() {
+    this.$http.get(`http://jsonplaceholder.typicode.com/posts/${this.id}`).then((data) => {
+      this.blog = data.body;
+    });
+  },
+}
+</script>
+
+<style>
+
+#single-blog {
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+</style>
