4ef760862ff5eedd9912e16ec07be08a71a2300b
waleedyousaf07 <waleedyousaf07@gmail.com>
2022-02-26 22:29:45 +0500

    creates a navbar

--- cliVersion/src/components/blog/BlogRoot.vue
+++ cliVersion/src/components/blog/BlogRoot.vue
@@ -1,25 +1,35 @@
 <template>
   <div>
-    <!-- <add-blog></add-blog> -->
-    <show-blogs></show-blogs>
-    <list-blogs></list-blogs>
+    <top-nav-bar :component="component" @changePage="updatePage($event)"></top-nav-bar>
+    <component :is="component">
+      <add-blog></add-blog>
+      <show-blogs></show-blogs>
+      <list-blogs></list-blogs>
+    </component>
   </div>
 </template>
 
 <script>
+import TopNavBar from '../topNavBar/TopNavBar.vue';
 import AddBlog from './AddBlog.vue';
 import ShowBlogs from './ShowBlogs.vue';
 import ListBlogs from './ListBlogs.vue';
 
 export default {
   components: {
+    'top-nav-bar': TopNavBar,
     'add-blog': AddBlog,
     'show-blogs': ShowBlogs,
     'list-blogs': ListBlogs,
   },
   data () {
     return {
-
+      component: 'show-blogs',
+    }
+  },
+  methods: {
+    updatePage: function (updatedPage) {
+      this.component = updatedPage;
     }
   },
 }
---
+++ cliVersion/src/components/topNavBar/TopNavBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div class="topnav">
+      <a :class="{active: component === 'show-blogs'}" href="#" @click="changeTitle('show-blogs')">Home</a>
+      <a :class="{active: component === 'add-blog'}" href="#" @click="changeTitle('add-blog')">Add Blog</a>
+      <a :class="{active: component === 'list-blogs'}" href="#" @click="changeTitle('list-blogs')">List Blog Titles</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['component'],
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    changeTitle: function (val) {
+      this.$emit('changePage', val);
+    }
+  },
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
